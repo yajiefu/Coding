470167235f1ccd4161e305ab76e68bb889eb16ba
yajiefu <yajiefu@outlook.com>
2019-05-27 16:44:12 +0800

    1

--- SwordPointToOffer/src/swordForOffer/Add65.java
+++ SwordPointToOffer/src/swordForOffer/Add65.java
@@ -1,4 +1,4 @@
-package java;
+package swordForOffer;
 /**
  * 写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
  * @author yajie
--- SwordPointToOffer/src/swordForOffer/StackWinMin.java
+++ SwordPointToOffer/src/swordForOffer/StackWinMin.java
@@ -1,4 +1,4 @@
-package java;
+package swordForOffer;
 
 import java.util.Stack;
 
