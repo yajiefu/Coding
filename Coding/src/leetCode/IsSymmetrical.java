cc0c02d86c3ecf8dd2dd1b3e88dc49c3f11ffc86
yajiefu <yajiefu@outlook.com>
2019-06-03 20:50:49 +0800

    101.对称二叉树

--- Coding/src/leetCode/IsSymmetrical.java
+++ Coding/src/leetCode/IsSymmetrical.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 import basic.TreeNode;
 
 /*
- * 题目：101对称二叉树
+ * 题目：101.对称二叉树
  * 描述：请实现一个函数，用来判断一颗二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
  */
 public class IsSymmetrical {
