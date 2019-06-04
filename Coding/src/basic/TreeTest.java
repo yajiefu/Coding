d43457cc5ce078ac9fa507ed41b1ec562476265f
yajiefu <yajiefu@outlook.com>
2019-06-04 13:26:03 +0800

    二叉树的前中后序遍历(递归+非递归) 层次遍历(常规+分行+之字形)

--- Coding/src/basic/TreeTest.java
+++ Coding/src/basic/TreeTest.java
@@ -2,6 +2,7 @@ package basic;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Stack;
 import basic.TreeNode;
@@ -30,11 +31,9 @@ public class TreeTest {
 
 	// 前序遍历-非递归1
 	/*
-	 *1.初始化一个空栈stack，申请一个节点空间pNode指向根节点（当前节点）
-	 *2.申请一个节点空间qNode，用来存放栈顶弹出的元素。
-	 *3.当p非空或者stack非空时，循环执行下面的操作
-	 *   如果p非空，访问该节点，并将节点p入栈，节点p再指向该节点的左孩子
-	 *   如果p为空，则弹出栈顶元素，将p指向该栈顶元素的右孩子。
+	 * 1.初始化一个空栈stack，申请一个节点空间pNode指向根节点（当前节点） 2.申请一个节点空间qNode，用来存放栈顶弹出的元素。
+	 * 3.当p非空或者stack非空时，循环执行下面的操作 如果p非空，访问该节点，并将节点p入栈，节点p再指向该节点的左孩子
+	 * 如果p为空，则弹出栈顶元素，将p指向该栈顶元素的右孩子。
 	 */
 	public static void preOrderUnRecur1(TreeNode root) {
 		Stack<TreeNode> stack = new Stack<>();
@@ -79,7 +78,7 @@ public class TreeTest {
 	}
 
 	// 中序遍历-递归
-	
+
 	public static void inOrderRecur(TreeNode root) {
 		if (root != null) {
 			inOrderRecur(root.left);
@@ -92,26 +91,27 @@ public class TreeTest {
 	/*
 	 * 1.申请一个新的栈stack，初始时，令变量cur=root
 	 * 2.现将cur入栈，对于以cur为根节点的整棵子树来说，依次把左边界压入栈中，即不停的令cur = cur.left,然后重复步骤2
-	 * 3.不断重复步骤2直到发现cur为空，此时从stack中弹出一个节点，记为node，打印node的值，并且让cur = node.right,然后重复步骤2
+	 * 3.不断重复步骤2直到发现cur为空，此时从stack中弹出一个节点，记为node，打印node的值，并且让cur =
+	 * node.right,然后重复步骤2
 	 */
 	public static void inOrderUnRecur(TreeNode root) {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
 		Stack<TreeNode> stack = new Stack<>();
 		TreeNode cur = root;
-		while(cur != null || !stack.isEmpty()) {
+		while (cur != null || !stack.isEmpty()) {
 			if (cur != null) {
 				stack.push(cur);
 				cur = cur.left;
-			}else {
+			} else {
 				// 当cur为空时，即左边界走到底了
 				TreeNode node = stack.pop();
 				System.out.print(node.val + " ");
 				cur = node.right;
 			}
 		}
-		
+
 	}
 
 	// 后序遍历-递归
@@ -123,13 +123,10 @@ public class TreeTest {
 		}
 	}
 
-	
 	/*
-	 * 方法1：用两个栈实现后序遍历
-	 * 1.申请一个栈stack1，然后将头节点root入栈stack1
+	 * 方法1：用两个栈实现后序遍历 1.申请一个栈stack1，然后将头节点root入栈stack1
 	 * 2.从stack1中弹出来的节点记为cur,然后依次将cur的左孩子和右孩子入栈stack1
-	 * 3.在整个过程中，每一次从stack1中弹出来的节点都放进stack2中
-	 * 4.不断重复步骤2和3，直到stack1中为空，过程停止
+	 * 3.在整个过程中，每一次从stack1中弹出来的节点都放进stack2中 4.不断重复步骤2和3，直到stack1中为空，过程停止
 	 * 5.从stack2总依次弹出节点，打印的顺序就是后序遍历的顺序
 	 */
 	// 后序遍历-非递归1
@@ -140,7 +137,7 @@ public class TreeTest {
 		Stack<TreeNode> stack1 = new Stack<>();
 		Stack<TreeNode> stack2 = new Stack<>();
 		stack1.push(root);
-		while(!stack1.isEmpty()) {
+		while (!stack1.isEmpty()) {
 			TreeNode cur = stack1.pop();
 			stack2.push(cur);
 			if (cur.left != null) {
@@ -150,38 +147,34 @@ public class TreeTest {
 				stack1.push(cur.right);
 			}
 		}
-		while(!stack2.isEmpty()) {
+		while (!stack2.isEmpty()) {
 			System.out.print(stack2.pop().val + " ");
 		}
 	}
-	
+
 	/*
-	 * 方法2：只用一个栈
-	 * 1.初始化一个空栈stack，将头节点root入栈，同时设置两个申请变量cur和pre，
-	 *   在这个流程中cur代表stack的栈顶节点，pre代表最近一次弹出并打印的节点。初始时，均为null。
-	 * 2.当stack非空时，循环执行下面的操作
-	 *     将栈顶节点置为当前节点cur,但是不从stack中弹出。
-	 *     如果当前节点没有左右孩子  或者 有左孩子或右孩子，但是已经被访问输出了：则访问该节点，并出栈，将其置为上一个访问的节点。
-	 *     如果不满足上面的条件的话，依次将右孩子和左孩子入栈。
+	 * 方法2：只用一个栈 1.初始化一个空栈stack，将头节点root入栈，同时设置两个申请变量cur和pre，
+	 * 在这个流程中cur代表stack的栈顶节点，pre代表最近一次弹出并打印的节点。初始时，均为null。 2.当stack非空时，循环执行下面的操作
+	 * 将栈顶节点置为当前节点cur,但是不从stack中弹出。 如果当前节点没有左右孩子  或者
+	 * 有左孩子或右孩子，但是已经被访问输出了：则访问该节点，并出栈，将其置为上一个访问的节点。 如果不满足上面的条件的话，依次将右孩子和左孩子入栈。
 	 */
 	// 后序遍历-非递归2
 	public static void postOrderUnRecur2(TreeNode root) {
 		if (root == null) {
-			return ;
+			return;
 		}
 		Stack<TreeNode> stack = new Stack<>();
 		stack.push(root);
 		// 存放栈顶元素
 		TreeNode cur = null;
 		TreeNode pre = null;
-		while(!stack.isEmpty()) {
+		while (!stack.isEmpty()) {
 			cur = stack.peek();
-			//如果当前节点没有左右孩子  或者 有左孩子或右孩子，但是已经被访问输出了：则访问该节点，并出栈，将其置为上一个访问的节点。
-			if ((cur.left == null && cur.right == null) ||
-					(pre != null && (cur.left == pre || cur.right == pre))) {
+			// 如果当前节点没有左右孩子  或者 有左孩子或右孩子，但是已经被访问输出了：则访问该节点，并出栈，将其置为上一个访问的节点。
+			if ((cur.left == null && cur.right == null) || (pre != null && (cur.left == pre || cur.right == pre))) {
 				System.out.print(stack.pop().val + " ");
 				pre = cur;
-			}else {
+			} else {
 				// 依次将cur的右孩子和左孩子入栈
 				if (cur.right != null) {
 					stack.push(cur.right);
@@ -190,12 +183,9 @@ public class TreeTest {
 					stack.push(cur.left);
 				}
 			}
-			
+
 		}
-		
-				
-		
-	
+
 	}
 
 	// 层次遍历
@@ -308,6 +298,72 @@ public class TreeTest {
 		}
 
 		return result;
+	}
+
+	// 之字形打印方法2
+	/*
+	 * 思路：相较于[把二叉树打印成多行]，该题的奇数行从左往右，偶数行从右往左。上题用的是队列，本题我们用双端队列
+	 *
+	 * 1.首先生成双端队列deque，将根节点从头部入队
+	 * 2.如果是从左往右的过程：一律从头部弹出节点，如果有孩子，从尾部依次进入左孩子和右孩子：如第1,3,5,层
+	 * 3.如果是从右往左的过程：一律从尾部弹出节点，如果有孩子，从头部依次进入右孩子和左孩子：如第2,4,6层
+	 * 
+	 */
+	public static ArrayList<ArrayList<Integer>> print(TreeNode pRoot) {
+		ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+		if (pRoot == null) {
+			return result;
+		}
+
+		Deque<TreeNode> deque = new LinkedList<>();
+		deque.addFirst(pRoot);
+		// flag 为true是奇数行
+		boolean flag = true;
+		// 本行剩余个数
+		int count = 1;
+		// 下一行个数
+		int nextCount = 0;
+		while (!deque.isEmpty()) {
+			// 如果是奇数行，从左往右打印，一律从头部弹出节点，如果有孩子，从尾部依次进入左孩子和右孩子
+			if (flag) {
+				TreeNode node = deque.pollFirst();
+				list.add(node.val);
+				count--;
+				if (node.left != null) {
+					deque.addLast(node.left);
+					nextCount++;
+				}
+				if (node.right != null) {
+					deque.addLast(node.right);
+					nextCount++;
+				}
+			} else {
+				// 如果是偶数行，从右往左打印，一律从尾部弹出节点，如果有孩子，从头部依次进入右孩子和左孩子
+				TreeNode node = deque.pollLast();
+				list.add(node.val);
+				count--;
+				if (node.right != null) {
+					deque.addFirst(node.right);
+					nextCount++;
+				}
+				if (node.left != null) {
+					deque.addFirst(node.left);
+					nextCount++;
+				}
+
+			}
+
+			if (count == 0) {
+				result.add(list);
+				list = new ArrayList<>();
+				flag = !flag;
+				count = nextCount;
+				nextCount = 0;
+			}
+		}
+
+		return result;
 
 	}
 
@@ -357,10 +413,20 @@ public class TreeTest {
 		System.out.println();
 		System.out.println("层次遍历-分行打印：");
 		levelOrderTraverse2(aNode);
-		System.out.println("层次遍历-之字形打印：");
+		System.out.println("层次遍历-之字形打印1：");
 		ArrayList<ArrayList<Integer>> result = levelOrderTraverse3(aNode);
 
 		for (ArrayList<Integer> arrayList : result) {
+			for (Integer val : arrayList) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("层次遍历-之字形打印2：");
+		ArrayList<ArrayList<Integer>> result1 = print(aNode);
+
+		for (ArrayList<Integer> arrayList : result1) {
 			for (Integer val : arrayList) {
 				System.out.print(val + " ");
 			}
