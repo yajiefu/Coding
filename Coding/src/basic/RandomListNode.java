404f0ba696dd5257211322e747c055d3aa577b53
yajiefu <yajiefu@outlook.com>
2019-06-10 21:52:09 +0800

    add

---
+++ Coding/src/basic/RandomListNode.java
@@ -0,0 +1,14 @@
+package basic;
+/*
+ * 复杂链表
+ */
+public class RandomListNode {
+
+	public int label;
+	public RandomListNode next = null;
+	public RandomListNode random = null;
+	
+	public RandomListNode(int label) {
+		this.label = label;
+	}
+}
