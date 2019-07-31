b1071fdbfee1b76d300e813f56bf2b8e6bde3257
yajiefu <yajiefu@outlook.com>
2019-07-31 16:22:27 +0800

    合并两个排序的链表

--- Coding/src/swordForOffer/MergeList.java
+++ Coding/src/swordForOffer/MergeList.java
@@ -69,6 +69,41 @@ public class MergeList {
     
         
     }
+    //方法2的另一种写法。将表头和中间的节点统一处理。
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if (l1 == null || l2 == null) {
+			return l1 == null ? l2 : l1;
+		}
+		
+		ListNode p1 = l1;
+		ListNode p2 = l2;
+		//新链表的表头
+		ListNode head = new ListNode(0);
+		ListNode cur = head;
+		while(p1 != null && p2 != null) {
+			if (p1.val <= p2.val) {
+				cur.next = p1;
+				p1 = p1.next;
+			}else {
+				cur.next = p2;
+				p2 = p2.next;
+			}
+			cur = cur.next;
+		}
+		
+		while(p1 != null) {
+			cur.next = p1;
+			cur = cur.next;
+			p1 = p1.next;
+		}
+		
+		while(p2 != null) {
+			cur.next = p2;
+			cur = cur.next;
+			p2 = p2.next;
+		}
+		return head.next;
+	}
     public static void main(String[] args) {
     	ListNode aListNode = new ListNode(0);
 		ListNode bListNode = new ListNode(5);
